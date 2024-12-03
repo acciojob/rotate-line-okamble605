1d98defc46f2ca7bbdadb8731c616374fe245187
Omkar Baburao Kamble <121740559+okamble605@users.noreply.github.com>
2024-12-03 19:51:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const line = document.getElementById('line');
+let angle = 0;
+
+function rotateLine() {
+  angle += 2; // Increment angle by 2 degrees
+  line.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`;
+  requestAnimationFrame(rotateLine); // Smooth animation
+}
+
+rotateLine();
